c419e5eb80012ce3ce876c2dbdbfc7e9ffd08ebe
G.M <47867339+GMLJ@users.noreply.github.com>
2021-08-06 11:54:54 +0200

    Zellan gallery

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,24 +1,26 @@
 // HAMBURGER TOGGLE
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-const nav = document.querySelector('nav');
-const canopy = document.querySelector('.imgCanopy');
-const body = document.querySelector('*');
-const secondLine = document.querySelector('.hamburgerSecondLine');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const nav = document.querySelector("nav");
+const canopy = document.querySelector(".imgCanopy");
+const body = document.querySelector("*");
+const secondLine = document.querySelector(".hamburgerSecondLine");
+const ionIcons = document.querySelectorAll(".ion-icons");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    })
-    nav.classList.toggle('open');
-    canopy.classList.toggle('open');
-    //canopy.classList.toggle('fade');
-    body.classList.toggle('open');
-    //rotation
-    secondLine.classList.toggle('hamburgerRotate');
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  ionIcons.forEach((icon) => {
+    icon.classList.toggle("undisplay");
+  });
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  nav.classList.toggle("open");
+  nav.classList.toggle("display");
+  canopy.classList.toggle("open");
+  //canopy.classList.toggle('fade');
+  body.classList.toggle("open");
+  //rotation
+  secondLine.classList.toggle("hamburgerRotate");
 });
-
-
-
